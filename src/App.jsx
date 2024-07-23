2d541e7bef78850bf5c4b404755480150f4d04e3
ziauddin-0987 <ziauddink100@gmail.com>
2024-07-24 00:39:34 +0500

    this is code input

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "antd";
+import { InputCompo } from "./inputComponent.jsx";
+
+function App() {
+  return (
+    <>
+      {/* <InputCompo /> */}
+      <Button type="primary">Primary Button</Button>
+    </>
+  );
+}
+
+export default App;

9a9430d9b87cb480f40177cd2ebd08e58ff375c7
ziauddin-0987 <ziauddink100@gmail.com>
2024-07-24 19:49:37 +0500

    this file in input access

--- src/inputComponent.jsx
+++ src/inputComponent.jsx
@@ -1,60 +1,79 @@
-TypeScript;
-JavaScript;
-import React, { useState } from "react";
-import { AutoComplete, Input } from "antd";
-const getRandomInt = (max, min = 0) =>
-  Math.floor(Math.random() * (max - min + 1)) + min;
-const searchResult = (query) =>
-  new Array(getRandomInt(5))
-    .join(".")
-    .split(".")
-    .map((_, idx) => {
-      const category = `${query}${idx}`;
-      return {
-        value: category,
-        label: (
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
-            <span>
-              Found {query} on{" "}
-              <a
-                href={`https://s.taobao.com/search?q=${query}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {category}
-              </a>
-            </span>
-            <span>{getRandomInt(200, 100)} results</span>
-          </div>
-        ),
-      };
-    });
-const InputCompo = () => {
-  const [options, setOptions] = useState([]);
-  const handleSearch = (value) => {
-    setOptions(value ? searchResult(value) : []);
-  };
-  const onSelect = (value) => {
-    console.log("onSelect", value);
-  };
-  return (
-    <AutoComplete
-      popupMatchSelectWidth={252}
-      style={{
-        width: 300,
-      }}
-      options={options}
-      onSelect={onSelect}
-      onSearch={handleSearch}
-      size="large"
-    >
-      <Input.Search size="large" placeholder="input here" enterButton />
-    </AutoComplete>
-  );
-};
-export default InputCompo;
+// import React, { useState } from "react";
+// import { AutoComplete, Input } from "antd";
+// // const getRandomInt = (max, min = 0) =>
+// //   Math.floor(Math.random() * (max - min + 1)) + min;
+// // const searchResult = (query) =>
+// //   new Array(getRandomInt(5))
+// //     .join(".")
+// //     .split(".")
+// //     .map((_, idx) => {
+// //       const category = `${query}${idx}`;
+// //       return {
+// //         value: category,
+// //         label: (
+// //           <div
+// //             style={{
+// //               display: "flex",
+// //               justifyContent: "space-between",
+// //             }}
+// //           >
+// //             <span>
+// //               Found {query} on{" "}
+// //               <a
+// //                 href={`https://s.taobao.com/search?q=${query}`}
+// //                 target="_blank"
+// //                 rel="noopener noreferrer"
+// //               >
+// //                 {category}
+// //               </a>
+// //             </span>
+// //             <span>{getRandomInt(200, 100)} results</span>
+// //           </div>
+// //         ),
+// //       };
+// //     });
+// const InputCompo = () => {
+//   // const [options, setOptions] = useState([]);
+//   // const handleSearch = (value) => {
+//   //   setOptions(value ? searchResult(value) : []);
+//   // };
+//   // const onSelect = (value) => {
+//   //   console.log("onSelect", value);
+//   // };
+//   // return (
+//   //   <AutoComplete
+//   //     popupMatchSelectWidth={252}
+//   //     style={{
+//   //       width: 300,
+//   //     }}
+//   //     options={options}
+//   //     onSelect={onSelect}
+//   //     onSearch={handleSearch}
+//   //     size="large"
+//   //   >
+//       <Input.Search size="large" placeholder="input here" enterButton />
+//     // </AutoComplete>
+//   // );
+// };
+// export default InputCompo;
+import React from "react";
+import { SearchOutlined } from "@ant-design/icons";
+import { Button, Input, Select, Space } from "antd";
+const { Search } = Input;
+const options = [
+  // {
+  //   value: "zhejiang",
+  //   label: "Zhejiang",
+  // },
+  {
+    value: "jiangsu",
+    label: "Jiangsu",
+  },
+];
+const Apps = () => (
+  <Space direction="vertical" size="middle">
+    <Input placeholder="print ToDo List" />
+
+  </Space>
+);
+export default Apps;

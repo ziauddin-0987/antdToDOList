01c5cd53318bc023d7fe68136263a2db2281a0ab
ziauddin-0987 <ziauddink100@gmail.com>
2024-07-24 19:53:51 +0500

    this file in input access

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button } from "antd";
-import { InputCompo } from "./inputComponent.jsx";
+import InputCompo from "./inputComponent.jsx";
 
 function App() {
   return (
     <>
-      {/* <InputCompo /> */}
-      <Button type="primary">Primary Button</Button>
+      <InputCompo />
+      <Button type="primary">submit</Button>
     </>
   );
 }
